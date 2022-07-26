4d2ecbe9f06918de04a42dcf8d02f0d1367da1da
Tarawut <tarawut.c@absolute.co.th>
2022-07-26 09:27:49 +0700

    Nes_login

--- app/helpers/settings_helper.rb
+++ app/helpers/settings_helper.rb
@@ -1,9 +1,9 @@
 module SettingsHelper
   def oauth_logins
     [
-      (:twitter if feature?(:twitter_login)),
-      (:facebook if feature?(:facebook_login)),
       (:google_oauth2 if feature?(:google_login)),
+      (:facebook if feature?(:facebook_login)),
+      (:twitter if feature?(:twitter_login)),
       (:wordpress_oauth2 if feature?(:wordpress_login))
     ].compact
   end
