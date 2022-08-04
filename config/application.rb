363e1f861f809a46f3d45e51abab4069ca00f346
Shiorin <junsuda.s@absolute.co.th>
2022-08-04 15:51:28 +0700

    fix timezone to Hanoi = Bangkok

--- config/application.rb
+++ config/application.rb
@@ -98,7 +98,7 @@ module Consul
 
     # Add lib to the autoload path
     config.autoload_paths << Rails.root.join("lib")
-    config.time_zone = "Madrid"
+    config.time_zone = "Hanoi"
     config.active_job.queue_adapter = :delayed_job
 
     # CONSUL specific custom overrides
